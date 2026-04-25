051463195e2676ec6791242be468998ef1ecbd90
BrandonDutt10 <bdogg2143@gmail.com>
2026-04-24 18:07:56 -0700

    Enhance modular script with comments and structure
    Refactor code to improve readability and add comments.

--- make-it-modular.js
+++ make-it-modular.js
@@ -1,8 +1,27 @@
+// Helps us safely build a path to our module file
 const path = require('path')
+
+// Load our custom module using an absolute-safe path
 const mymodule = require(path.join(__dirname, 'mymodule'))
 
-mymodule(process.argv[2], process.argv[3], function (err, data) {
-  if (err) return console.error(err)
+/*
+  Command-line inputs:
+  process.argv[2] = folder path
+  process.argv[3] = file extension filter
+*/
+const folder = process.argv[2]
+const ext = process.argv[3]
 
-  data.forEach(file => console.log(file))
+// Call the module and handle results
+mymodule(folder, ext, function (err, data) {
+
+  // If there’s an error, print it and stop
+  if (err) {
+    return console.error(err)
+  }
+
+  // Print each file on its own line
+  data.forEach(function (file) {
+    console.log(file)
+  })
 })
